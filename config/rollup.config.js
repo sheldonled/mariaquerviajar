3ae479c747b42bb7a943896a77765e3a1819e067
Sheldon Led <sheldonled@gmail.com>
2021-11-01 18:13:32 +0000

    Delete WP theme

--- config/rollup.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  input: 'src/js/main.js',
-  output:{
-    file: 'dist/js/app.js',
-    format: 'iife'
-  },
-  name: 'mqv'
-};
--- src/js/image.js
+++
@@ -1,5 +0,0 @@
-const loadAll = () => Array.prototype.slice
-                      .call(document.querySelectorAll("img[data-src]"))
-                      .forEach(img => img.setAttribute('src', img.getAttribute('data-src')));
-
-export default {loadAll};
