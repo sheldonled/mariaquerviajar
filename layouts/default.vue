9ac6ca6f0e88293d8e7a1944c35e54e53b57fbb7
Sheldon Led <sheldonled@gmail.com>
2021-11-09 09:12:24 +0000

    Add Default nuxt Layout

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<script>
+import Header from "../components/Header.vue";
+
+export default {
+  name: "DefaultLayout",
+  components: {
+    Header,
+  },
+};
+</script>
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Be Vietnam", sans-serif;
+}
+body {
+  margin: 0;
+}
+.wrapper {
+  width: 100%;
+  max-width: 68.75rem;
+  margin: 0 auto;
+}
+:root {
+  --yellow-bg-color: #e7a603;
+}
+</style>
