b880a6b3a27a126f323e64b77edf1adffe8e45e7
Sheldon Led <sheldonled@gmail.com>
2021-11-28 22:55:53 +0000

    Fix mobile menu on safari

--- components/Header.vue
+++ components/Header.vue
@@ -32,7 +32,7 @@ export default {
             @click="menuToggle"
             aria-label="Main Menu"
           >
-            <svg viewBox="0 0 100 100">
+            <svg width="48" height="48" viewBox="0 0 100 100">
               <path
                 class="line line1"
                 d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
@@ -78,6 +78,7 @@ export default {
   top: 0;
   left: 0;
   right: 0;
+
   @media (min-width: 40rem) {
     position: fixed;
     box-shadow: $header-footer-box-shadow;
@@ -111,38 +112,32 @@ export default {
   .line {
     fill: none;
     stroke: $white;
-    stroke-width: 6;
+    stroke-width: 6px;
     transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1),
       stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
   }
   .line1 {
     stroke-dasharray: 60 207;
-    stroke-width: 6;
   }
   .line2 {
     stroke-dasharray: 60 60;
-    stroke-width: 6;
   }
   .line3 {
     stroke-dasharray: 60 207;
-    stroke-width: 6;
   }
 
   &.opened {
     .line1 {
       stroke-dasharray: 90 207;
       stroke-dashoffset: -134;
-      stroke-width: 6;
     }
     .line2 {
       stroke-dasharray: 1 60;
       stroke-dashoffset: -30;
-      stroke-width: 6;
     }
     .line3 {
       stroke-dasharray: 90 207;
       stroke-dashoffset: -134;
-      stroke-width: 6;
     }
   }
 
