ef6c1a9d30950dd36626607830b19905651f5f35
Sheldon Led <sheldonled@gmail.com>
2021-11-29 15:35:39 +0000

    Add placeholder for new website

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Hello world!</h1>
+  <main>
+    <h1>Maria Quer Viajar</h1>
+
+    <p>Um novo site está sendo construído</p>
+    <p>
+      Por enquanto, acompanhe as novidades no
+      <a href="https://instagram.com/mariaquerviajar">Instagram</a> ou no
+      <a href="https://youtube.com/mariaquerviajar"> YouTube</a>
+    </p>
+  </main>
 </template>
+<style>
+body {
+  text-align: center;
+}
+</style>
