422fe25023863754ace6bebe9afc123e857fbd94
Sheldon Led <sheldonled@gmail.com>
2023-01-01 13:31:31 +0000

    Add sanity and upgrade dependencies

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Maria Quer Viajar
 
 This is [https://mariaquerviajar.com](mariaquerviajar.com) website, built with nuxt.
+
+Built with:
+- Nuxt 3: https://nuxt.com/v3 | https://nuxtjs.org/announcements/nuxt3-rc/
+- Sanity: https://www.sanity.io/docs/connect-your-content-to-nuxt3
+
+To Run it:
+- git clone git@github.com:sheldonled/mariaquerviajar.git
+- yarn install
+- yarn dev
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,50 @@
-import { defineNuxtConfig } from "nuxt";
-
-export default defineNuxtConfig({
-  // https://v3.nuxtjs.org/api/configuration/nuxt.config#ssr
+export default {
+  // https://nuxtjs.org/docs/configuration-glossary/configuration-ssr/
   ssr: false,
+  // https://nuxtjs.org/docs/configuration-glossary/configuration-target/
   target: "static",
-  // Global page headers: https://v3.nuxtjs.org/api/configuration/nuxt.config#head
-  app: {
-    head: {
-      title: "Maria Quer Viajar",
-      htmlAttrs: {
-        lang: "pt-BR",
-      },
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { hid: "description", name: "description", content: "" },
-        { name: "format-detection", content: "telephone=no" },
-      ],
-      link: [
-        { rel: "icon", type: "image/svg", href: "/favicon.svg" },
-        { rel: "preconnect", href: "https://fonts.googleapis.com" },
-        {
-          rel: "preconnect",
-          href: "https://fonts.gstatic.com",
-          crossorigin: true,
-        },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Be+Vietnam&display=swap",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap",
-        },
-      ],
+  // Global page headers: https://nuxtjs.org/docs/configuration-glossary/configuration-head/
+  head: {
+    title: "Maria Quer Viajar",
+    htmlAttrs: {
+      lang: "pt-BR",
     },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Maria Quer Viajar" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [
+      { rel: "icon", type: "image/svg", href: "/favicon.svg" },
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossorigin: true,
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Be+Vietnam&display=swap",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap",
+      },
+    ],
   },
-  // Global CSS: https://v3.nuxtjs.org/api/configuration/nuxt.config#css
+  // Global CSS: https://nuxtjs.org/docs/configuration-glossary/configuration-css
   css: [],
-  // Auto import components: https://v3.nuxtjs.org/api/configuration/nuxt.config#components
+  // Auto import components: https://nuxtjs.org/docs/configuration-glossary/configuration-components
   components: {
     dirs: ["~/components"],
+  },
+  // Modules: https://nuxtjs.org/docs/configuration-glossary/configuration-modules
+  modules: ["@nuxtjs/sanity"],
+  // Sanity CMS: https://www.sanity.io/docs/connect-your-content-to-nuxt3
+  sanity: {
+    projectId: "qhw09lqc",
+    apiVersion: "2022-03-25",
   },
   vite: {
     css: {
@@ -51,4 +55,4 @@ export default defineNuxtConfig({
       },
     },
   },
-});
+};
